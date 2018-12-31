9526a4b0f4cf2a86c9b0a2fb9f3682a329cf81ef
Jacob Hixon <jacobhixon47@gmail.com>
2018-12-31 03:53:44 -0600

    display working timer

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,15 +18,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        <div>
           {children}
         </div>
       </>
@@ -34,6 +26,7 @@ const Layout = ({ children }) => (
   />
 )
 
+// <Header siteTitle={data.site.siteMetadata.title} />
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,75 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class IndexPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: '',
+      hours: '',
+      minutes: '',
+      seconds: '',
+      newYearsDay: new Date(`January 1, ${new Date().getFullYear() + 1} 00:00:00`)
+    };
+
+    this.getTime = this.getTime.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.getTime();
+    }, 1000);
+  }
+
+  getTime() {
+    let days, hours, minutes, seconds;
+    let current = new Date();
+    let timeLeft = parseInt((this.state.newYearsDay - current) / 1000);
+    // get days, adjust timeLeft
+    days = parseInt(timeLeft / 86400);
+    timeLeft = (timeLeft % 86400);
+    // get hours, adjust timeLeft
+    hours = ("0" + parseInt(timeLeft / 3600)).slice(-2);
+    timeLeft = (timeLeft % 3600);
+    // get minutes, adjust timeLeft
+    minutes = ("0" + parseInt(timeLeft / 60)).slice(-2);
+    timeLeft = (timeLeft % 60);
+    // get seconds
+    seconds = ("0" + parseInt(timeLeft)).slice(-2);
+    // update state with time left
+    this.setState({days: days, hours: hours, minutes: minutes, seconds: seconds});
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
+        <div style={{display: 'flex', justifyContent: 'space-around', alignItems: 'center', width: '100vw', height: '100vh'}}>
+          <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+            <h1>Days</h1>
+            <h2>{this.state.days}</h2>
+          </div>
+          <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+            <h1>Hours</h1>
+            <h2>{this.state.hours}</h2>
+          </div>
+          <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+            <h1>Mins</h1>
+            <h2>{this.state.minutes}</h2>
+          </div>
+          <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+            <h1>Seconds</h1>
+            <h2>{this.state.seconds}</h2>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
+}
 
 export default IndexPage
